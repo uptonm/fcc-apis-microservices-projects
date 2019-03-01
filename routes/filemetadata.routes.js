7c22275f15f61381e1681ae26768ccfa1fffaea6
Mike Upton <uptonm@wit.edu>
2019-03-01 13:40:51 -0500

    Added file metadata parser

--- routes/filemetadata.routes.js
+++ routes/filemetadata.routes.js
@@ -1,8 +1,8 @@
-var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 module.exports = router => {
-  router.post('/file/upload', upload.single('file'), (req, res) => {
+  router.post("/file/upload", upload.single("file"), (req, res) => {
     res.status(200).send({
       name: req.file.originalname,
       type: req.file.mimetype,
